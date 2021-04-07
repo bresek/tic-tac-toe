e1b7b598cc690756472a3e0dbc77c8891f6cc5c0
Ben Resek <benresek@Bens-MacBook-Air.local>
2021-04-07 18:08:10 -0400

    changes turn correctly. Added max turn function. Changed gameboard to 2d array. now board array is updated for each move

--- index.js
+++ index.js
@@ -22,34 +22,40 @@ const gameboard = (() => {
     //access board through dom 
     const boardContain =  document.querySelector('#container')
     // initialize empty board 
-    let board = [' ','','',
-                '','','',
-                '',' ',' '];
+    let board = [['','',''],
+                ['','',''],
+                ['','','']];
     const render = () =>{
        
        for (let i = 0; i < board.length; i++){
+           for (let j = 0; j < board[0].length; j++){
             const div = document.createElement('div')
             div.setAttribute('class','cell')
-            div.setAttribute('data-index',i)
-            div.innerText = board[i]
+            div.setAttribute('data-row',i)
+            div.setAttribute('data-col', j)
+            div.innerText = board[i][j]
             boardContain.appendChild(div)
+           }
        }
        
+       
     }
-    const getMove = (marker) =>{
-        console.log(marker)
-        boardContain.addEventListener('click', (event) => {
-            if (event.target.getAttribute("class") === 'cell'){
-                
+    const placeSymbol = () =>{
+       boardContain.addEventListener('click', (event)=>{
+           console.log('click')
+           if (event.target.getAttribute("class") === 'cell' && event.target.innerText === ''){
+                //figure out whos turn it is function 
+                let marker = flow.determineTurn()
                 event.target.innerText = marker
+                gameboard.board[event.target.getAttribute('data-row')][event.target.getAttribute('data-col')] = marker
                 console.log(flow.move)
+                //check for win function goes here? 
+                flow.checkForWin(gameboard.board)
+                flow.nextTurn()
             }
-            
-
-
-        })
+       })
     }
-    return {board, render, getMove}
+    return {board, render, placeSymbol}
 })()
 
 
@@ -60,31 +66,52 @@ const gameboard = (() => {
 //module for tracking flow of the game 
 const flow = (() => {
    
-    let move = 0;
-    let gameOver = false
+    
     //initialize players. Not sure if it should go in here. 
-    const player1 = player('1', '❌');
-    const player2 = player('2','🅾️');
+        let move = 0;
+        let gameOver = false
+        const player1 = player('1', '❌');
+        const player2 = player('2','🅾️');
+    
     const nextTurn = () => {
-        move++;
-        console.log('next turn, move: '+ move)
+        if (move <8){
+            move++;
+            console.log('next turn, move: '+ move)
+        }else{
+            alert('game over- tie!')
+        }
+        
         
     }
-    const play = () =>{
+    const startGame = () =>{
         gameboard.render()
+        if (!gameOver){
+            determineTurn()
+            gameboard.placeSymbol()
+        }
+    }
+    const determineTurn = () =>{
+        
+        let symbol = ''
         if (move % 2 === 0){
-            gameboard.getMove(player1.getSymbol())
+            symbol = player1.getSymbol() 
 
         }else{
-            gameboard.getMove(player2.getSymbol())
+             symbol = player2.getSymbol() 
         }
-        nextTurn();
+        return symbol
         
       
     }
+    const checkForWin = (moves) =>{
+        console.log(moves)
+    }
 
     
-    return {move, nextTurn ,play}
+    return {move, nextTurn ,determineTurn, startGame, checkForWin}
 })()
 
-flow.play()
+flow.startGame()
+
+
+// winning logic 
